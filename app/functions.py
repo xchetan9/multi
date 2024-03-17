fbecb8b561dd5e26f0f44387abf6585e750e60a5
Chetan Singh <122537861+xchetan9@users.noreply.github.com>
2024-03-17 15:30:25 +0530

    Create functions.py

---
+++ app/functions.py
@@ -0,0 +1,34 @@
+# app/functions.py
+import requests
+
+api_configs = {
+    "sendcm": {
+        "key": "233675oewkgzcw80s21ibm",
+        "url": "https://send.cm/api/upload/url"
+    },
+    "mediacm": {
+        "key": "8475ait8co2ohk0mvhaf",
+        "url": "https://media.cm/api/upload/url"
+    },
+    "filemoonapi": {
+        "key": "49874pa34xr1tm3fkssf7",
+        "url": "https://filemoonapi.com/api/remote/add"
+    },
+    "streamwish": {
+        "key": "13582uc1lasxn8w58ygse",
+        "url": "https://api.streamwish.com/api/upload/url"
+    }
+}
+
+def upload_to_api(api_config, url):
+    try:
+        full_url = f"{api_config['url']}?key={api_config['key']}&url={url}"
+        response = requests.get(full_url)
+        if response.status_code == 200:
+            data = response.json()
+            if data.get('result'):
+                return data['result']['filecode']
+        return 0
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        return 0
