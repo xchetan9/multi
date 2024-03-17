f3ee3c0dfa0f06a06d21f6b0c4dd2884a439088d
Chetan Singh <122537861+xchetan9@users.noreply.github.com>
2024-03-17 15:30:49 +0530

    Create routes.py

---
+++ app/routes.py
@@ -0,0 +1,17 @@
+# app/routes.py
+from flask import Blueprint, request, jsonify
+from .functions import upload_to_api, api_configs
+
+api = Blueprint('api', __name__)
+
+@api.route('/upload', methods=['POST'])
+def upload_file():
+    url_to_upload = request.json.get('url')
+    if not url_to_upload:
+        return jsonify({"error": "URL not provided"}), 400
+
+    file_codes = {}
+    for api_name, api_config in api_configs.items():
+        file_codes[api_name] = upload_to_api(api_config, url_to_upload)
+
+    return jsonify(file_codes)
